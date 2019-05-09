40ccce25d35e4666e775f4ec1ca35d9447e96fda
cezar.carneiro <cezargcarneiro@gmail.com>
2019-05-09 13:58:28 +0100

    refactor SolverTest

--- src/test/java/com/mobiquityinc/packer/SolverTest.java
+++ src/test/java/com/mobiquityinc/packer/SolverTest.java
@@ -1,6 +1,5 @@
 package com.mobiquityinc.packer;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
@@ -44,6 +43,7 @@ public class SolverTest {
 		6 : (1,2,€10) (2,2,€10) (3,5,€20) 1,2 should prioritize combination of items that weight less over single item that weights more #
 		
 	 */
+	
 	public static Stream<Arguments> dataSet() {
 		return Stream.of(
 				Arguments.of(
@@ -51,177 +51,113 @@ public class SolverTest {
 						/* expected */
 						Collections.emptyList(),
 						/* param */
-						new TestCase( 
-								new BigDecimal("1"),
-								Collections.emptyList())),
+						testCase("1", Collections.emptyList())),
 				/***************************************************************************/
 				Arguments.of(
 						"Should return an empty list for a test case with only one item that is above the package limit",
 						/* expected */
 						Collections.emptyList(),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("6"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "6","10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should return an empty list for a test case with multiple items above the package limit",
 						/* expected */
 						Collections.emptyList(),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("6"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("6"), new BigDecimal("10"))))),
-				
+						testCase("5", listOfThings(thing(1, "6", "10"), thing(2, "6", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick one item out of one item below package limit",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null)),
+						listOfThings(thing(1)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("4"), new BigDecimal(10))))),
+						testCase("5", listOfThings(thing(1, "4", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick one item out of one item that weights exactly the package limit",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null)),
+						listOfThings(thing(1)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("4"), new BigDecimal(10))))),
+						testCase("5", listOfThings(thing(1, "4", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick one item if the test case has one item that is in the package limit and another item that is above the package limit",
 						/* expected */
-						Arrays.asList(new Thing(2, null, null)),
+						listOfThings(thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("6"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("5"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "6", "10"), thing(2, "5", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick multiple items if the test case has multiple items below the limit and multiple items above the limit",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null), new Thing(2, null, null)),
+						listOfThings(thing(1), thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("2"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("2"), new BigDecimal("10")), 
-										new Thing(3, new BigDecimal("6"), new BigDecimal("10")), 
-										new Thing(4, new BigDecimal("6"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "2", "10"), thing(2, "2", "10"), thing(3, "6", "10"), thing(4, "6", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick all items if all items fit together in the package",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null), new Thing(2, null, null), new Thing(3, null, null)),
+						listOfThings(thing(1), thing(2), thing(3)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("6"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("1"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("2"), new BigDecimal("10")), 
-										new Thing(3, new BigDecimal("3"), new BigDecimal("10"))))),
+						testCase("6", listOfThings(thing(1, "1", "10"), thing(2, "2", "10"), thing(3, "3", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick the lighest item from multiple items with the same value",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null)),
+						listOfThings(thing(1)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("3"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("4"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "3", "10"), thing(2, "4", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick the lighest item from multiple items with the same value, and the lightest item appears last",
 						/* expected */
-						Arrays.asList(new Thing(2, null, null)),
+						listOfThings(thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("4"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("3"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "4", "10"), thing(2, "3", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick the most valuable item from multiple items that have the same weight",
 						/* expected */
-						Arrays.asList(new Thing(2, null, null)),
+						listOfThings(thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("5"), new BigDecimal("9")), 
-										new Thing(2, new BigDecimal("5"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "5", "9"), thing(2, "5", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick the most valuable item from multiple items that have the same weight",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null)),
+						listOfThings(thing(1)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("5"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("5"), new BigDecimal("9"))))),
+						testCase("5",
+								listOfThings(thing(1, "5", "10"), thing(2, "5", "9")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick the most valuable item from multiple items that have the same weight, and the most valuable item appears last",
 						/* expected */
-						Arrays.asList(new Thing(2, null, null)),
+						listOfThings(thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("5"), new BigDecimal("9")), 
-										new Thing(2, new BigDecimal("5"), new BigDecimal("10"))))),
+						testCase("5", listOfThings(thing(1, "5", "9"), thing(2, "5", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick a heavier but more valuable item over a lighter and less valuable item",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null)),
+						listOfThings(thing(1)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("5"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("4"), new BigDecimal("4")), 
-										new Thing(2, new BigDecimal("3"), new BigDecimal("3"))))),
+						testCase("5", listOfThings(thing(1, "4", "4"), thing(2, "3", "3")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick a combination of items that together have more value over a single item with the same weight but less value",
 						/* expected */
-						Arrays.asList(new Thing(2, null, null), new Thing(3, null, null)),
+						listOfThings(thing(2), thing(3)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("6"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("6"), new BigDecimal("15")), 
-										new Thing(2, new BigDecimal("3"), new BigDecimal("10")), 
-										new Thing(3, new BigDecimal("3"), new BigDecimal("10"))))),
+						testCase("6", listOfThings(thing(1, "6", "15"), thing(2, "3", "10"), thing(3, "3", "10")))),
 				/***************************************************************************/
 				Arguments.of(
 						"Should pick a combination of items that together weight less but have the same value of a single item that weights more and have the same value",
 						/* expected */
-						Arrays.asList(new Thing(1, null, null), new Thing(2, null, null)),
+						listOfThings(thing(1), thing(2)),
 						/* param */
-						new TestCase( 
-								new BigDecimal("6"),
-								Arrays.asList(
-										new Thing(1, new BigDecimal("2"), new BigDecimal("10")), 
-										new Thing(2, new BigDecimal("2"), new BigDecimal("10")), 
-										new Thing(3, new BigDecimal("5"), new BigDecimal("20")))))
+						testCase("6", listOfThings(thing(1, "2", "10"), thing(2, "2", "10"), thing(3, "5", "20"))))
 				);
 	}
 	
@@ -245,4 +181,20 @@ public class SolverTest {
 		List<Thing> result = solver.solve(param);
         assertTrue(message, expected.equals(result) );
     }
+	
+	public static Thing thing(Integer index) {
+		return thing(index, null, null);
+	}
+	
+	public static Thing thing(Integer index, String weight, String value) {
+		return new Thing(index, weight == null ? null : new BigDecimal(weight), value == null ? null : new BigDecimal(value));
+	}
+	
+	public static TestCase testCase(String capacity, List<Thing> things) {
+		return new TestCase( new BigDecimal(capacity), things);
+	}
+	
+	public static List<Thing> listOfThings(Thing... t) {
+		return Arrays.asList(t);
+	}
 }
